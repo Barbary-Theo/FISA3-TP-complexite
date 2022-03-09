5674087ac02d014363862bab0ed111ba9d70d8c0
Barbary-Theo <theo.barbary25@gmail.com>
2022-03-09 15:31:01 +0100

    (complexité): did fibonnaci, execpt petit c)

--- tp1.py
+++ tp1.py
@@ -36,10 +36,10 @@ def getDataHanoi(n):
         print(" --> Calculation done successfully : for n = ", i, " in ", execution)
 
 
-def displayData():
+def displayData(list):
     ax.clear()
 
-    x, y = all_n, all_nb_occurence
+    x, y = all_n, list
     ax.plot(x, y)
 
     canvas.draw()
@@ -57,12 +57,53 @@ def hanoi(n):
     return HanoiTour(n, 'A', 'B', 'C', 0)
 
 
-def prog():
+def main_exo_1():
     n = int(input("Jusqu'à la valeur : "))
     getDataHanoi(n)
-    displayData()
+    displayData(all_nb_occurence)
     root.mainloop()
 
 
+def suite_recurrente_iter(n):
+    res = []
+    for i in range(n + 1):
+        if i == 0 or i == 1:
+            res.append(1)
+        else:
+            res.append(res[i - 1] + res[i - 2])
+
+    return res[-1]
+
+
+def suite_recurrente_recu(n):
+
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return suite_recurrente_recu(n - 1) + suite_recurrente_recu(n - 2)
+
+
+def getData_suite_recu(n, f):
+
+    for i in range(1, n):
+        all_n.append(i)
+
+        start = time.time()
+        f(i)
+        end = time.time()
+
+        execution = end - start
+        execution_time.append(execution)
+        #print(" --> Calculation done successfully : for n = ", i, " in ", execution)
+    print("done")
+
+def main_exo_2():
+    n = int(input("Jusqu'à la valeur : "))
+
+    getData_suite_recu(n, suite_recurrente_recu)
+    #getData_suite_recu(n, suite_recurrente_iter)
+    displayData(execution_time)
+    root.mainloop()
+
 if __name__ == "__main__":
-    prog()
+    main_exo_1()
